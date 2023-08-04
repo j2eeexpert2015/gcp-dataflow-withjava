ccba7391c6fc992720472bc75264c9f31ee27a2e
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-08-04 13:50:23 +0530

    Added changes

--- src/main/resources/composer/dags/invoke_dataflow_multiple_jobs.py
+++ src/main/resources/composer/dags/invoke_dataflow_multiple_jobs.py
@@ -1,21 +1,27 @@
 import datetime
 from airflow import models
-from airflow.contrib.operators.dataflow_operator import DataflowTemplateOperator
+from airflow.contrib.operators.dataflow_operator import DataFlowJavaOperator
 from airflow.utils.dates import days_ago
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryInsertJobOperator,
+    BigQueryIntervalCheckOperator,
+    BigQueryValueCheckOperator,
+)
 
 bucket_path = "gs://gcpsampleall/gcp_java/gcp-dataflow-withjava-1.0-SNAPSHOT-bundled.jar"
-project_id = "sanguine-anthem-393416"
+gcp_project_id = "sanguine-anthem-393416"
+dataset_id="demo"
 
 default_args = {
     'start_date': days_ago(1),
     'dataflow_default_options': {
-        'project': project_id,
+        'project': gcp_project_id,
         'tempLocation': 'gs://gcpsampleall/dataflow_common/temp'
     }
 }
 
 with models.DAG(
-        'dataflowfromcomposer',
+        'composermultipletasks',
         schedule_interval=datetime.timedelta(days=1),
         default_args=default_args) as dag:
 
@@ -32,6 +38,6 @@ with models.DAG(
             "query": "CALL `{}.{}.populate_customer`(); ".format(gcp_project_id,dataset_id),
             "useLegacySql": False,
         }
-    },
+    })
 
     t1 >> t2
